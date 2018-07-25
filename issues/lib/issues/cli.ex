9a9b9ed47377b82dc60815a8f5b11f4208669326
Fabio Kreusch <fabiokr@gmail.com>
2018-07-25 15:09:43 -0300

    CLI

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -7,7 +7,36 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+         i1["created_at"] >= i2["created_at"]
+       end)
   end
 
   @doc """
