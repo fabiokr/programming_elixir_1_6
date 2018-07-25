9cd4809bc3e50c8eb64e8836d0743d17556ae9e8
Fabio Kreusch <fabiokr@gmail.com>
2018-07-25 18:10:39 -0300

    Docs

--- issues/mix.exs
+++ issues/mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      name: "Issues",
       escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.6",
@@ -24,6 +25,8 @@ defmodule Issues.MixProject do
     [
       { :httpoison, "~> 1.0.0" },
       { :poison, "~> 3.1" },
+      { :ex_doc, "~> 0.18.1" },
+      { :earmark , "~> 1.2.4"},
     ]
   end
 
